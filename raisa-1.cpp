632907e0f7c6acd70fdeba23feedd10abf4d2d61
Raisa Nuraida <111173409+RaisaNuraida@users.noreply.github.com>
2022-09-19 06:01:52 -0700

    Add files via upload

---
+++ raisa-1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Nama  : Raisa Nuraida"<<endl;
+	cout << "Nama  : X RPL 2"<<endl;
+	cout << "Nilai :"<<endl;return 0;
+}
---
+++ raisa-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	string nama;
+	 int umur;
+  
+ 	 cout << "nama :";
+	 cin >> nama;
+  	 cout << "umur :";
+	 cin >> umur;
+}
